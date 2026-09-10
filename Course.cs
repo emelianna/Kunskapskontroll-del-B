4b4fe4c6fd3f65e17dae01643dc160800be92cf4
Emelianna Axelsson <emelianna.axelsson@gmail.com>
2026-09-10 13:39:30 +0200

    Fler metoder och lite fix i Course.cs

--- Course.cs
+++ Course.cs
@@ -1,9 +1,9 @@
 class Course
 {
-    public string Name;     //fält med klassens egenskaper
+    public string Name;     //fält 
     public int MaxSeats;    
 
-    public Course(string name, int maxSeats) //Konstruktorn, returnerar inget
+    public Course(string name, int maxSeats) //Konstruktorn, returnerar inget. Fyller i objektets fält med startvärden
     {
         Name = name;
         MaxSeats = maxSeats;
@@ -14,15 +14,38 @@ class Course
 
 
 
-    public void Enroll(Student newStudent)
+    public void Enroll(Student newStudent) //Lägger till student
     {
-         if (!students.Contains(newStudent) && MaxSeats - students.Count > 0)
-
+         if (!students.Contains(newStudent) && MaxSeats - students.Count > 0) //Bara om personen inte redan är med
+                                                                               //och om det finns platser kvar  
         {
             students.Add(newStudent);
+            // oldStudent.Join(this); //Senare, när metoden Join är skapad i Student-klassen
+           
+        }
+    }
+    public void Remove(Student oldStudent) //Tar bort student
+    {
+         if (students.Contains(oldStudent))
+
+        {
+              students.Remove(oldStudent);
             // newStudent.Leave(this); //Senare, när metoden Leave är skapad i Student-klassen
         }
     }
+
+    public void RollCall()
+    {
+        Console.WriteLine("Studerande i denna kurs är:");
+
+        foreach (Student student in students)
+        {
+           Console.WriteLine($"- {student.Name}");
+        }
+
+    
+}
+    
 
     public override string ToString()
     {
